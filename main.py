91cbb16c11c76ba983072f29555960adfdc630ad
Sviatoslav Abramov <abr4m0vs@gmail.com>
2023-01-15 14:58:07 +0100

    Add preprocessing for Age and Pclass columns, and correlation displaying between columns, display classification report for models

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from sklearn import tree, model_selection, naive_bayes
+from sklearn import tree, model_selection, naive_bayes, ensemble, preprocessing
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import ensemble
-from sklearn import preprocessing
 import xgboost as xgb
 from sklearn.model_selection import train_test_split, ShuffleSplit, LearningCurveDisplay
+from sklearn.metrics import classification_report
 import joblib
 import pandas as pd
 
@@ -12,27 +11,40 @@ filename = 'weights/titanic_survivor_grad_boost_classifier.pkl'
 
 
 def convert_csv_to_arrays(path_to_csv: str, train: bool):
-    original_df = pd.read_csv(path_to_csv, sep=',', header=0)\
-        .drop('Ticket', axis=1)
+    original_df = pd.read_csv(path_to_csv,
+                              sep=',',
+                              header=0).drop('Ticket', axis=1)
     original_df.interpolate(inplace=True)
 
+    original_df['Sex'] = original_df['Sex'].map(lambda x: 1 if x == 'male' and x is not None else 0)
+
+    label_encoder = preprocessing.LabelEncoder()
+    names_encoder = preprocessing.OrdinalEncoder()
+    cabin_encoder = preprocessing.LabelEncoder()
+    psclass_encoder = preprocessing.OneHotEncoder(sparse=False)
+    age_encoder = preprocessing.OneHotEncoder(sparse=False)
+
+    original_df['Embarked'] = label_encoder.fit_transform(original_df['Embarked'])
+    original_df['Name'] = names_encoder.fit_transform(original_df['Name'].values.reshape(-1, 1))
+    original_df['Cabin'] = cabin_encoder.fit_transform(original_df['Cabin'].values.reshape(-1, 1))
+    original_df['Pclass'] = psclass_encoder.fit_transform(original_df['Cabin'].values.reshape(-1, 1))
+    original_df['Age'] = age_encoder.fit_transform(original_df['Cabin'].values.reshape(-1, 1))
+
+    corr = original_df.corr().round(2)
+    pd.set_option('display.max_colwidth', None)
+    with pd.option_context('display.max_colwidth', None,
+                           'display.max_columns', None,
+                           'display.max_rows', None):
+        print(corr)
     if train:
         out_df = original_df['Survived'].copy()
         in_df = original_df.drop('Survived', axis=1)
     else:
         in_df = original_df.copy()
+    in_df = in_df.drop('PassengerId', axis=1)
+    X = in_df.to_numpy()
 
-    in_df = in_df
-    in_df['Sex'] = in_df['Sex'].map(lambda x: 1 if x == 'male' else 0)
-    label_encoder = preprocessing.LabelEncoder()
-    names_encoder = preprocessing.OrdinalEncoder()
-    cabin_encoder = preprocessing.LabelEncoder()
-
-    in_df['Embarked'] = label_encoder.fit_transform(in_df['Embarked'])
-    in_df['Name'] = names_encoder.fit_transform(in_df['Name'].values.reshape(-1, 1))
-    in_df['Cabin'] = cabin_encoder.fit_transform(in_df['Cabin'].values.reshape(-1, 1))
     print(in_df.head(5))
-    X = in_df.drop('PassengerId', axis=1).to_numpy()
     if train:
         y = out_df.to_numpy()
         return y, X
@@ -58,8 +70,8 @@ if __name__ == '__main__':
     rf_clf = model_selection.GridSearchCV(ensemble.RandomForestClassifier(), scoring='roc_auc', param_grid=param_grid)
     gb_clf.fit(x_train, y_train)
     rf_clf.fit(x_train, y_train)
-    print(f'Accuracy of gb is:           {100 * gb_clf.score(x_test, y_test):.2f}%\n')
-    print(f'Accuracy of rf is:           {100 * rf_clf.score(x_test, y_test):.2f}%\n')
+    print(f'Classification report of gb is:\n{classification_report(y_test, gb_clf.predict(x_test))}')
+    print(f'Classification report of rf is:\n{classification_report(y_test, rf_clf.predict(x_test))}')
 
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
     common_params = {
@@ -79,6 +91,6 @@ if __name__ == '__main__':
         ax[ax_idx].legend(handles[:2], ["Training Score", "Test Score"])
         ax[ax_idx].set_title(f"Learning Curve for {estimator.__class__.__name__}")
 
-    plt.show()
+    plt.savefig()
 
 
